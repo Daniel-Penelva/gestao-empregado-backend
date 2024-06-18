d5ad2efb20e81f2444e515f91455558ce86b0f19
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-18 18:28:46 -0300

    criando projeto springboot

---
+++ src/main/java/com/api/gestao_empregado_backend/GestaoEmpregadoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.gestao_empregado_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoEmpregadoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoEmpregadoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/gestao_empregado_backend/GestaoEmpregadoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gestao_empregado_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoEmpregadoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
