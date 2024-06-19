b048a8d15776bb3bfeff9204bf584dd471010ac5
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-18 22:40:40 -0300

    Criando Cross origin

--- src/main/java/com/api/gestao_empregado_backend/controller/EmpregadoController.java
+++ src/main/java/com/api/gestao_empregado_backend/controller/EmpregadoController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.api.gestao_empregado_backend.repository.EmpregadoRepository;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class EmpregadoController {
 
     @Autowired
